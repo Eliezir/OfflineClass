bd4c5c050a02cf116abf7e8c77c38876d88a887e
Eliezir <eliezirneto@gmail.com>
2026-06-30 18:02:26 -0300

    refactor(avatar): promove o construtor de avatar para o pacote @offlineclass/avatar

--- packages/avatar/src/index.ts
+++ packages/avatar/src/index.ts
@@ -1,5 +1,9 @@
 export { Avatar } from './avatar'
 export type { AvatarProps } from './avatar'
+export { AvatarBuilder } from './AvatarBuilder'
+export type { AvatarBuilderProps } from './AvatarBuilder'
+export { AvatarEditorModal } from './AvatarEditorModal'
+export type { AvatarEditorModalProps } from './AvatarEditorModal'
 export { avatarSvg } from './dicebear'
 export {
   DEFAULT_AVATAR,
